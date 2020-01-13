a3fbad253e70d5061a94504f1ca32c2a36357853
valentinpi <valenpi@posteo.de>
2020-01-13 18:42:13 +0100

    Small comment fix and two more macros

--- mips.asm
+++ mips.asm
@@ -5,7 +5,21 @@
     lw \reg1, 4($sp)
 .endm
 
-; Examples
+.macro push reg
+    addi $sp, $sp, 4
+    sw \reg, 0($sp)
+.endm
+
+.macro pop reg
+    lw \reg, 0($sp)
+    # Zero-out stack top
+    sw $zero, 0($sp)
+    # There is no subi instruction in the current version
+    # of the MIPS instruction set
+    addi $sp, $sp, -4
+.endm
+
+# Examples
 foo:
     mov $v0, $zero
     addi $v0, $v0, 1
@@ -13,4 +27,6 @@ foo:
     addi $v0, $v0, 1
     addi $v0, $v0, 1
     addi $v0, $v0, 1
+    push $v0
+    pop $v0
     jal $ra
