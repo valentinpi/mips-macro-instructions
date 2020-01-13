78701f74487e942b6fc2c755c2fd4c001f418773
valentinpi <valentinpickel@gmx.de>
2020-01-13 18:55:28 +0100

    Two more instructions and an outline of the implementation goals

--- mips.asm
+++ mips.asm
@@ -1,8 +1,8 @@
 .globl foo
 
-.macro mov reg1, reg2
-    sw \reg2, 4($sp)
-    lw \reg1, 4($sp)
+.macro mov dst, src
+    sw \src, 4($sp)
+    lw \dst, 4($sp)
 .endm
 
 .macro push reg
@@ -10,14 +10,40 @@
     sw \reg, 0($sp)
 .endm
 
-.macro pop reg
-    lw \reg, 0($sp)
+.macro pop dst
+    lw \dst, 0($sp)
     # Zero-out stack top
     sw $zero, 0($sp)
     # There is no subi instruction in the current version
     # of the MIPS instruction set
     addi $sp, $sp, -4
 .endm
+
+.macro multt dst reg1 reg2
+    mult \reg1, \reg2
+    mflo \dst
+.endm
+
+# div
+
+# mod
+
+# not
+
+.macro clear dst
+    # Alternative:
+    # mov \dst, $zero
+    sw $zero, 4($sp)
+    lw \dst, 4($sp)
+.endm
+
+# ror
+
+# rol
+
+# bgt
+
+# ble
 
 # Examples
 foo:
@@ -29,4 +55,8 @@ foo:
     addi $v0, $v0, 1
     push $v0
     pop $v0
+    multt $v0, $v0, $v0
+    multt $v0, $v0, $v0
+    multt $v0, $v0, $v0
+    clear $v0
     jal $ra
